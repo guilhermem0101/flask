2debf0380b18c441b961fc0062703fbd859bec5a
guilhermem0101 <guilherme.sou@hotmail.com>
2023-08-03 11:45:57 -0300

    att

--- main.py
+++ main.py
@@ -6,5 +6,10 @@ app = Flask(__name__)
 def index():
   return render_template('index.html')
 
+@app.route('/as', methods=['GET'])
+def indexs():
+  x=[1,2,3]
+  return x
+
 if __name__ == '__main__':
   app.run(port=5000)
