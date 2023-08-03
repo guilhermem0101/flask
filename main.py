e7c8ba49f5f6513cc5d8c46f6c62e4edd3339271
guilhermem0101 <guilherme.sou@hotmail.com>
2023-08-03 14:19:03 -0300

    add conection with database

--- main.py
+++ main.py
@@ -1,42 +1,25 @@
 from flask import Flask, render_template
-from dotenv import load_dotenv
-load_dotenv()
-import os
-import MySQLdb
+
 import mysql.connector
 app = Flask(__name__)
 
 
 
-def conecta_db2():
+def conecta_db():
   conn = mysql.connector.connect(
     host="aws.connect.psdb.cloud",
     database="analises",
-    user="boc36q84eq64ebmrjqbs",
-    password="pscale_pw_gpxgFaKmTnOUcs5vHQbBLI3U3uVuRJrBmS6wbKUEkbg"
+    user="j45yzh42xjey1qfjo8m2",
+    password="pscale_pw_34bY4RaLDwJFUzJ4TuLRkCPguLPfbN0G80tHBZha1db"
 
   )
   return conn
 
 
-def conecta_db():
-  
-  connection = MySQLdb.connect(
-    host= "aws.connect.psdb.cloud",
-    user="rlk7h67rkbqblfoopzzb",
-    passwd= "pscale_pw_Q0K5ORUMEoO1R80hZ0lwp0NN7rTa8y0YQCuyIe1maTc",
-    db= "analises",
-    autocommit = True,
-    ssl_mode = "VERIFY_IDENTITY",
-    ssl      = {
-      "ca": "./etc/ssl/cert.pem"
-    }
-  )
-  return connection
 
 # Consulta global
 def consulta(sql):
-  con = conecta_db2()
+  con = conecta_db()
   cur = con.cursor()
   cur.execute(sql)
   recset = cur.fetchall()
