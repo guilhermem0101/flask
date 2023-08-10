c6b0f463ab7050bb7434fadf241c9d291a8d9574
guilhermem0101 <guilherme.sou@hotmail.com>
2023-08-10 16:27:26 -0300

    construção de serie temporal

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, send_file
+from flask import Flask, send_file, request
 import os
 import io
 import mysql.connector
@@ -7,7 +7,7 @@ import matplotlib.pyplot as plt
 app = Flask(__name__)
 
 
-import os
+
 def conecta_db():
   conn = mysql.connector.connect(
     host=os.getenv("host"),
@@ -57,23 +57,35 @@ def getAllProdutos():
   return data
 
 
+
+
+
+
 @app.route('/vendas', methods=['GET'])
-def getSerie():
+def getSeries():
+  intervalo = request.args.get('intervalo')
   url = "https://raw.githubusercontent.com/guilhermem0101/ml-book-exemplos/main/dados_transformados.csv"
   df = pd.read_csv(url)
-  df = df[df['Order Date'] != 'Order Date']
+  
+  # Certifique-se de que o nome da coluna está correto
+  df.rename(columns={'Data': 'Order Date'}, inplace=True)
+  
+  # Converte a coluna 'Order Date' para o tipo de data
+  df['Order Date'] = pd.to_datetime(df['Order Date'])
+  
+  # Define 'Order Date' como o índice do DataFrame
   df.set_index('Order Date', inplace=True)
-  serie_temporal_dia = df.resample('D')['Sales'].sum()
+  
+  serie_temporal = df.resample(intervalo)['Sales'].sum()
+  
   plt.figure(figsize=(10, 6))
-  serie_temporal_dia.plot(kind='line', marker='o')
+  serie_temporal.plot(kind='line', marker='o')
   img_buffer = io.BytesIO()
   plt.savefig(img_buffer, format='png')
   img_buffer.seek(0)
 
   # Retorna a imagem diretamente como resposta
   return send_file(img_buffer, mimetype='image/png')
-
-
 
 
 
